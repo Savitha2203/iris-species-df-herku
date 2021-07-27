8c78d15f42b9192e2111f80d855f2eaa5f842722
Savitha2203 <87774546+Savitha2203@users.noreply.github.com>
2021-07-27 11:56:42 +0530

    Update improved_iris_app.py

--- improved_iris_app.py
+++ improved_iris_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 #import seaborn as sns
 import streamlit as st
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 #from sklearn.svm import SVC
 #from sklearn.linear_model import LogisticRegression  
 #from sklearn.ensemble import RandomForestClassifier
